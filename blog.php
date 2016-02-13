63de29fb463e2c9fbd166ea9bc1decf796198452
dekulb <srnksk2_kznnark@yahoo.co.jp>
2016-02-13 16:47:21 +0900

    homepage_repository

---
+++ blog.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DEKURUBOU | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <link rel="stylesheet" href="css/animsition.min.css">
+
+    <script src="js/vendor/modernizr.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/myfunction.js"></script>
+    <script type="text/javascript">
+    LinkAnimation();
+    </script>
+    <!-- animsition js -->
+    <script src="js/jquery.animsition.min.js"></script>
+   </head>
+  <body>
+         
+      <div class="row">
+        <div class="large-12 columns">
+          <h1>でくるぼう<small>　同人サークル「でくるぼう」の公式サイトです！</small></h1>
+          <hr/>
+        </div>
+      </div>
+     
+       
+     
+     
+       
+            <div class="row">
+        <div class="large-12 columns">
+          <ul class="button-group">
+              <li><a href=<?php echo TOP_PAGE; ?> class="button animsition-link">トップ</a></li>
+             <li><a href=<?php echo BLOG_PAGE; ?> class="button animsition-link">活動情報</a></li>
+             <li><a href=<?php echo PICTURE_PAGE; ?> class="button animsition-link">ギャラリー</a></li>
+             <li><a href=<?php echo WEBSTUDY_PAGE; ?> class="button animsition-link">Web開発勉強</a></li>
+             <li><a href=<?php echo ASK_PAGE; ?> class="button animsition-link">問い合わせ</a></li>
+
+          </ul>
+        </div>
+      </div>
+       
+     
+     
+       
+     
+      <div class="row animsition">
+     
+         
+        <div class="large-9 columns" role="content">
+     
+          <article>
+     
+            <h3><a name="li20150217" href="#" >HP開設！！</a></h3>
+            <h6>Written by <a href="#">DEKU </a>2015/02/17</h6>
+     
+            <div class="row">
+              <div class="large-6 columns">
+                <p>ホームページ開設しました！！ここでは、pixivやイベントでの活動情報を更新していきます。 </p>
+                
+              </div>
+              <div class="large-6 columns">
+                <img src="http://placehold.it/400x240&text=[img]"/>
+              </div>
+            </div>
+            
+                 </article>
+     
+          <hr/>
+          
+          
+        </div>
+     
+         
+     
+     
+         
+     
+        <aside class="large-3 columns">
+     
+          <h5>過去の記事</h5>
+          <ul class="side-nav">
+            <li><a href="#">2015年2月</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+          </ul>
+     
+          <div class="panel">
+                      <h4 style="color: #e7e7e7;
+text-shadow:
+-1px -1px 1px rgba(0, 0, 0, 0.4),
+1px 1px 1px rgba(255, 255, 255, 0.5);"><i class="fi-social-twitter" style="color:#55ccff;"></i><span style="color:#cce7ff;"> DEKUのTwitter</span></h4>
+            <p><a class="twitter-timeline" href="https://twitter.com/dekulb" data-widget-id="569532023841124352">@dekulbさんのツイート</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
+<h4 style="color: #e7e7e7;
+text-shadow:
+-1px -1px 1px rgba(0, 0, 0, 0.4),
+1px 1px 1px rgba(255, 255, 255, 0.5); display:table;"><span style="display:table-cell; vertical-align:middle"><img src="img/pixiv.png" style="width:23px; height:23px; vertical-align:top"></span>&nbsp;<span style="color:#77aaff;">DEKUのPixiv</span></h4>
+<p><a href="http://www.pixiv.net/member.php?id=3150511"><img src="img/pixiv_user3150511.gif"></a></p>
+<iframe style="background:transparent;" width="170" height="200" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" src="http://embed.pixiv.net/blogparts.php?md=m&id=dekulb104_80f165ef94be797606618cc6c337ee41&logoColor=0x258FB8&bgColor=0xFFFFFF&btnColor=0xFFFFFF&allowColor=0x258FB8&nanameColor=0xEDEDED&wakuColor=0x999999&tabColor=0xFF9900&fontColor=0x000000"></iframe></p>
+          </div>
+     
+        </aside>
+     
+         
+      </div>
+     
+       
+     
+     
+       
+     
+       <footer class="row">
+        <div class="large-12 columns">
+          <hr/>
+          <div class="row">
+            <div class="large-6 columns">
+            <p>© 2015 DEKURUBOU. All rights reserved</p>
+            </div>
+            <div class="large-6 columns">
+              <ul class="inline-list right">
+            <li><a class="animsition-link" href=<?php echo TOP_PAGE; ?>>トップ</a></li>
+            <li><a class="animsition-link" href=<?php echo BLOG_PAGE; ?>>活動情報</a></li>
+            <li><a class="animsition-link" href=<?php echo PICTURE_PAGE; ?>>ギャラリー</a></li>
+            <li><a class="animsition-link" href=<?php echo WEB_STUDY_PAGE; ?>>Web開発勉強</a></li>
+            <li><a class="animsition-link" href=<?php echo ASK_PAGE; ?>>問い合わせ</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    
+    
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,207 @@
+<?php
+require_once('define.inc');
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DEKURUBOU | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <!-- animsition CSS -->
+    <link rel="stylesheet" href="css/animsition.min.css">
+    <link rel="stylesheet" href="font/foundation-icons.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/myfunction.js"></script>
+    <script type="text/javascript">
+    FirstAnimation();
+    LinkAnimation();
+    </script>
+    <!-- animsition js -->
+    <script src="js/jquery.animsition.min.js"></script>
+<!-- <script type="text/javascript">
+$(document).ready(function() {
+  $(".animsition").animsition({
+  
+    inClass               :   'fade-in-left',
+    outClass              :   'fade-out-left',
+    inDuration            :    1500,
+    outDuration           :    800,
+    linkElement           :   '.animsition-link',
+    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+    loading               :    true,
+    loadingParentElement  :   'body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ],
+    //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    
+    overlay               :   false,
+    
+    overlayClass          :   'animsition-overlay-slide',
+    overlayParentElement  :   'body'
+  });
+});
+</script>
+-->
+    <script type="text/javascript" src="js/introtzikas.js"></script>
+<!--
+<script type="text/javascript">
+    $(document).ready(function() {
+$().introtzikas({
+line: '#fff', //ラインの色
+speedwidth: 800, //幅の移動完了スピード
+speedheight: 300, //高さの移動完了スピード
+speedopacity:400,
+bg: '#333', //背景色
+lineheight: 5
+}       
+       );
+    });
+ </script>
+-->
+
+</head>
+  <body >
+
+
+      <div class="row">
+        <div class="large-12 columns">
+          <h1>でくるぼう<small>　同人サークル「でくるぼう」の公式サイトです！</small></h1>
+          <hr/>
+        </div>
+      </div>
+
+           <div class="row">
+        <div class="large-12 columns">
+          <ul class="button-group">
+              <li><a href=<?php echo TOP_PAGE; ?> class="button animsition-link">トップ</a></li>
+             <li><a href=<?php echo BLOG_PAGE; ?> class="button animsition-link">活動情報</a></li>
+             <li><a href=<?php echo PICTURE_PAGE; ?> class="button animsition-link">ギャラリー</a></li>
+             <li><a href=<?php echo WEBSTUDY_PAGE; ?> class="button animsition-link">Web開発勉強</a></li>
+             <li><a href=<?php echo ASK_PAGE; ?> class="button animsition-link">問い合わせ</a></li>
+
+          </ul>
+        </div>
+      </div>
+     
+     
+     
+      <div class="row animsition">
+     
+        <div class="large-8 columns" role="content">
+            <p><img src="http://placehold.it/1000x600&text=[banner img]"/></p>
+            <h2>更新情報</h2>
+            <div id="information">
+                <table class="information" border="0">
+                <colgroup>
+                <col class="moon8">
+                </colgroup>
+                <colgroup>
+                <col class="moon10">
+                </colgroup>
+                <colgroup>
+                <col class="moon15">
+                </colgroup>
+                <tbody>
+                <tr>
+                    <td class="img"><img src="img/ani-new6.gif" /></td>
+                    <td class="date"><span>2015.02.17</span></td>
+                    <td class="topic">
+                    <div>
+                    <a class="animsition-link" href="blog.html#li20150217">ホームページを開設しました！！</a>
+                    </div>
+                    </td>
+                </tr>
+<!--                <tr>
+                    <td class="img"></td>
+                    <td class="date"><span>2015.02.20</span></td>
+                    <td class="topic">
+                    <div>
+                    <br />
+                    <a class="animsition-link" href="#">C85参加情報</a>
+                    </div>
+                    </td>
+                </tr>
+                <tr>
+-->
+                                </tbody>
+                </table>
+            </div>   
+                
+        </div>
+         
+     
+     
+         
+     
+        <aside class="large-4 columns">
+     
+          <h5>ギャラリー</h5>
+          <ul class="small-block-grid-3">
+            <li><a class="animsition-link" href="#"><img src="img/misaka_chibi120.png"/></a></li>
+            <li><a class="animsition-link" href="#"><img src="img/index_chibi120.png"/></a></li>
+            <li><a class="animsition-link" href="#"><img src="img/sirika_C87120.png"/></a></li>
+            <li><a class="animsition-link" href="#"><img src="img/marnie120.png"/></a></li>
+            <li><a class="animsition-link" href="#"><img src="img/limited120.png"/></a></li>
+            <li><a class="animsition-link" href="#"><img src="img/cokaholic_chan120.png"/></a></li>
+          </ul>
+               
+          <div class="panel">
+            <h4 style="color: #e7e7e7;
+text-shadow:
+-1px -1px 1px rgba(0, 0, 0, 0.4),
+1px 1px 1px rgba(255, 255, 255, 0.5);"><i class="fi-social-twitter" style="color:#55ccff;"></i><span style="color:#cce7ff;"> DEKUのTwitter</span></h4>
+            <p><a class="twitter-timeline" href="https://twitter.com/dekulb" data-widget-id="569532023841124352">@dekulbさんのツイート</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
+<h4 style="color: #e7e7e7;
+text-shadow:
+-1px -1px 1px rgba(0, 0, 0, 0.4),
+1px 1px 1px rgba(255, 255, 255, 0.5); display:table;"><span style="display:table-cell; vertical-align:middle"><img src="img/pixiv.png" style="width:23px; height:23px; vertical-align:top"></span>&nbsp;<span style="color:#77aaff;">DEKUのPixiv</span></h4>
+<p><a href="http://www.pixiv.net/member.php?id=3150511"><img src="img/pixiv_user3150511.gif"></a></p>
+<iframe style="background:transparent;" width="170" height="200" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" src="http://embed.pixiv.net/blogparts.php?md=m&id=dekulb104_80f165ef94be797606618cc6c337ee41&logoColor=0x258FB8&bgColor=0xFFFFFF&btnColor=0xFFFFFF&allowColor=0x258FB8&nanameColor=0xEDEDED&wakuColor=0x999999&tabColor=0xFF9900&fontColor=0x000000"></iframe></p>
+          </div>
+     
+        </aside>
+     
+         
+      </div>
+     
+       
+     
+     
+       
+     
+      <footer class="row">
+        <div class="large-12 columns">
+          <hr/>
+          <div class="row">
+            <div class="large-6 columns">
+            <p>© 2015 DEKURUBOU. All rights reserved</p>
+            </div>
+            <div class="large-6 columns">
+              <ul class="inline-list right">
+            <li><a class="animsition-link" href=<?php echo TOP_PAGE; ?>>トップ</a></li>
+            <li><a class="animsition-link" href=<?php echo BLOG_PAGE; ?>>活動情報</a></li>
+            <li><a class="animsition-link" href=<?php echo PICTURE_PAGE; ?>>ギャラリー</a></li>
+            <li><a class="animsition-link" href=<?php echo WEB_STUDY_PAGE; ?>>Web開発勉強</a></li>
+            <li><a class="animsition-link" href=<?php echo ASK_PAGE; ?>>問い合わせ</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    
+    
+<!--    <script src="js/vendor/jquery.js"></script> -->
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
